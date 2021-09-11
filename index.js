fb04eba2a8fafa7b332f709defe84d0e947556f6
Savita Verma <svitaverma10@gmail.com>
2021-09-11 12:59:28 +0530

    fix: convert in num & add condition

--- index.js
+++ index.js
@@ -7,30 +7,34 @@ var outputMessage = document.querySelector("#output-message");
 tellMeBtn.addEventListener('click', submitHandler);
 
 function submitHandler() {
-    var ip = initialPrice.value;
-    var qty = quantityOfstock.value;
-    var curr = currentPrice.value;
+    var ip = Number(initialPrice.value);
+    var qty = Number(quantityOfstock.value);
+    var curr = Number(currentPrice.value);
     calculateProfitAndLoss(ip, qty, curr);
 
 }
 
 function calculateProfitAndLoss(initial, quantity, current) {
-    if (current > initial) {
-        var profit = (current - initial) * quantity;
-        var profitPercentage = (profit / initial) * 100;
-        showMessage(`The profit is ${profit} and the ${profitPercentage}%`);
-    } else if (initial > current) {
-        var loss = (initial - current) * quantity;
-        var lossPercentage = (loss / initial) * 100;
-        showMessage(`The loss is ${loss} and the ${lossPercentage}%`);
+    if (current > 0 && initial > 0) {
+        if (current > initial) {
+            var profit = (current - initial) * quantity;
+            var profitPercentage = (profit / initial) * 100;
+            showMessage(`The profit is ${profit} and the ${profitPercentage}%`);
+        } else if (initial > current) {
+            var loss = (initial - current) * quantity;
+            var lossPercentage = (loss / initial) * 100;
+            showMessage(`The loss is ${loss} and the ${lossPercentage}%`);
 
-    } else {
-        showMessage("No pain no gain, no gain no pain");
+        } else {
+            showMessage("No pain no gain, no gain no pain");
+        }
+    }else{
+        showMessage("Enter valid number");
     }
+
 
 }
 
 function showMessage(message) {
     outputMessage.innerHTML = message;
 }
-
